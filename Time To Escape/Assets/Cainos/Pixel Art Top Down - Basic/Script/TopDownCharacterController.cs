19c4d1819e0ce3919e2e9f3bec8209c2436ba1cf
Adnane <adnane.dinaoui@epita.fr>
2022-06-02 20:31:35 +0200

    inchallah ça passe

---
+++ Time To Escape/Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cainos.PixelArtTopDown_Basic
+{
+    public class TopDownCharacterController : MonoBehaviour
+    {
+        public float speed;
+
+        private Animator animator;
+        public GameObject diamond;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+
+        private void Update()
+        {
+            Vector2 dir = Vector2.zero;
+            if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                dir.x = -10;
+                animator.SetInteger("Direction", 3);
+            }
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                dir.x = 10;
+                animator.SetInteger("Direction", 2);
+            }
+
+            if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.UpArrow))
+            {
+                dir.y = 10;
+                animator.SetInteger("Direction", 1);
+            }
+            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                dir.y = -10;
+                animator.SetInteger("Direction", 0);
+            }
+
+            dir.Normalize();
+            animator.SetBool("IsMoving", dir.magnitude > 0);
+
+            GetComponent<Rigidbody2D>().velocity = speed * dir;
+
+        }
+
+        void OnTriggerEnter2D(Collider2D target)
+        {
+            if (target.tag == "Diamond")
+            {
+
+                Destroy(target.gameObject);
+
+            }
+        }
+    }
+}
